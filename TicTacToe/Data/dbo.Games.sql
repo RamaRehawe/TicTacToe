41a4a1093389563c419c3ac1dc12656f8b84f15d
RamaRehawe <ramarehawe51@gmail.com>
2023-12-19 20:19:12 +0300

    Finishing Database

---
+++ TicTacToe/Data/dbo.Games.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Games]
+(
+	[GameId] INT NOT NULL PRIMARY KEY, 
+    [Player1ID] INT NOT NULL, 
+    [Player2ID] INT NOT NULL, 
+    [WinnerID] INT NULL, 
+    [DatePlay] DATE NOT NULL
+)
---
+++ TicTacToe/Data/dbo.Moves.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Moves] (
+    [MoveId]      INT      NOT NULL,
+    [GameId]      INT      NOT NULL,
+    [PlayerId]    INT      NOT NULL,
+    [MoveNumber]  INT      NOT NULL,
+    [RowIndex]    INT      NOT NULL,
+    [ColumnIndex] INT      NOT NULL,
+    [Symbol]      CHAR (2) NOT NULL,
+    PRIMARY KEY CLUSTERED ([MoveId] ASC),
+    CONSTRAINT [Pord] FOREIGN KEY ([GameId]) REFERENCES [dbo].[Games] ([GameId]), 
+    CONSTRAINT [Player] FOREIGN KEY ([PlayerId]) REFERENCES [Players]([PlayerId])
+);
+
